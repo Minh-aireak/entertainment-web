5cae540361761cdbfee9414762a26053823671cd
Minh-aireak <aireakminh205@gmail.com>
2025-08-09 23:18:51 +0700

    Update 1 ( 09/08/2025 )

---
+++ notification-service/src/main/java/com/MyProject/notification/notification_service/service/EmailService.java
@@ -0,0 +1,41 @@
+package com.MyProject.notification.notification_service.service;
+
+import com.MyProject.notification.notification_service.dto.request.EmailRequest;
+import com.MyProject.notification.notification_service.dto.request.SendEmailRequest;
+import com.MyProject.notification.notification_service.dto.request.Sender;
+import com.MyProject.notification.notification_service.dto.response.EmailResponse;
+import com.MyProject.notification.notification_service.exception.AppException;
+import com.MyProject.notification.notification_service.exception.ErrorCode;
+import com.MyProject.notification.notification_service.repository.httpclient.EmailClient;
+import feign.FeignException;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class EmailService {
+    EmailClient emailClient;
+    String apiKey = "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";
+
+    public EmailResponse senEmail(SendEmailRequest request){
+        EmailRequest emailRequest = EmailRequest.builder()
+                .sender(Sender.builder()
+                        .name("aireak.com")
+                        .email("tuanminh18122005@gmail.com")
+                        .build())
+                .to(request.getTo())
+                .subject(request.getSubject())
+                .htmlContent(request.getHtmlContent())
+                .build();
+        try {
+            return emailClient.sendEmail(apiKey, emailRequest);
+        } catch (FeignException exception){
+            throw new AppException(ErrorCode.CANNOT_SEND_EMAIL);
+        }
+    }
+}
