e4d7ffb52dcf836fb7c36c1248e3541800182d91
Minh-aireak <aireakminh205@gmail.com>
2025-08-11 12:21:20 +0700

    Update notification-service ( 11/08/2025 )

--- notification-service/src/main/java/com/MyProject/notification/notification_service/controller/EmailController.java
+++ notification-service/src/main/java/com/MyProject/notification/notification_service/controller/EmailController.java
@@ -7,12 +7,15 @@ import com.MyProject.notification.notification_service.service.EmailService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class EmailController {
     EmailService emailService;
@@ -20,7 +23,12 @@ public class EmailController {
     @PostMapping("/email/send")
     ApiResponse<EmailResponse> emailResponse(@RequestBody SendEmailRequest request){
         return ApiResponse.<EmailResponse>builder()
-                .result(emailService.senEmail(request))
+                .result(emailService.sendEmail(request))
                 .build();
+    }
+
+    @KafkaListener(topics = "onboard-successful")
+    public void listen(String message){
+        log.info("Message : {}", message);
     }
 }
