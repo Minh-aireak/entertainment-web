1100aef17ed5030d017291f6cab4517ef8c10f9b
Minh-aireak <aireakminh205@gmail.com>
2025-09-01 18:09:58 +0700

    Delete file/src/main/java/com/MyProject/file directory

--- file/src/main/java/com/MyProject/file/dto/response/FileDownload.java
+++
@@ -1,6 +0,0 @@
-package com.MyProject.file.dto.response;
-
-import org.springframework.core.io.Resource;
-
-public record FileDownload(String contentType, Resource resource) {
-}
--- file/src/main/java/com/MyProject/file/repository/FileMgmtRepository.java
+++
@@ -1,9 +0,0 @@
-package com.MyProject.file.repository;
-
-import com.MyProject.file.entity.FileMgmt;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FileMgmtRepository extends MongoRepository<FileMgmt, String> {
-}
