c7690b9ed4f7e9dbb531cebaa7e162ae681dc816
Nguyen Tuan Minh <aireakminh205@gmail.com>
2026-08-13 21:15:46 +0700

    chore: add .gitattributes, update .gitignore and bump maven wrapper

--- mvnw.cmd
+++ mvnw.cmd
@@ -40,6 +40,7 @@
 @SET __MVNW_ARG0_NAME__=
 @SET MVNW_USERNAME=
 @SET MVNW_PASSWORD=
+@SET JAVA_HOME=D:\JDK_24
 @IF NOT "%__MVNW_CMD__%"=="" ("%__MVNW_CMD__%" %*)
 @echo Cannot start maven from wrapper >&2 && exit /b 1
 @GOTO :EOF
